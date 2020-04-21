2472f5a0fb6ab566b29d01910c184a0b7398d94a
ZhaoRui-A <516911867@qq.com>
2020-04-21 20:34:55 +0800

    lab5

---
+++ lab5/Lab5设计文档/Lab 5 设计文档.md
@@ -0,0 +1,41 @@
+# Lab 5 设计文档
+
+## 1、Part I
+
+![part1codes](..\Lab5设计文档\part1codes.png)
+
+![part1page1](..\Lab5设计文档\part1page1.png)
+
+![part1page2](..\Lab5设计文档\part1page2.png)
+
+### 知识点
+
+- 正则表达式。通过?<=方式匹配以“?/&”+“name=”开头的字符串，并通过.*?(?=&|$)来限制末尾为"&"或空。
+- match方法。该方法未匹配到将返回null，便于自定义未匹配返回值。
+
+## 2、Part II
+
+![part2codes](..\Lab5设计文档\part2codes.png)
+
+![part2page1](..\Lab5设计文档\part2page1.jpg)
+
+![part2page2](..\Lab5设计文档\part2page2.png)
+
+### 知识点
+
+- Date 对象的使用（包括一些具体方法）。
+- setInterval方法的使用。第一个参数为自写函数，通过if/else子句实现了加倍、加倍次数、加倍时间等相关要求，第二个参数为间隔时间，设置为5000毫秒（5秒）。
+
+## 3、Part III
+
+![part3codes](..\Lab5设计文档\part3codes.png)
+
+![part3page](..\Lab5设计文档\part3page.png)
+
+### 知识点
+
+- 对象的熟练使用。几年前在leetcode刷题时碰到过类似题目，当时是用数组解决，但JS中用对象思想解决更为便捷。
+
+## 其他
+
+无
--- lab5/lab05.js
+++ lab5/lab05.js
@@ -10,15 +10,30 @@ let url_submit = document.getElementById("url_submit");
 let url_result = document.getElementById("url-result");
 url_submit.addEventListener('click',showWindowHref);
 function showWindowHref(){
-
+// let temp=/(?<=[?&]name=).*?(?=&)/.exec(url.value)[0];
+    let temp=url.value.match(/(?<=[?&]name=).*?(?=&|$)/);
+    if(temp==null) {url_result.value="No Name Here"}
+    else url_result.value=temp;
 }
 //2. 每隔五秒运行一次函数直到某一整分钟停止，比如从20:55:45运行到20:56:00停止；或者运行10次，先到的为准。从1开始每过五秒，输入框内数值翻倍。初始值为1。
 //注意：你可以在函数 timeTest内部 和 timeTest外部 写代码使得该功能实现。
 //与设置时间相关的函数可以上网查找。
-
 //提示：mul为html中id为"mul"的元素对象，可直接通过mul.value获得其内的输入值。
 let mul = document.getElementById("mul");
+mul.value=1;
+timeTest();
 function timeTest(){
+    let minutes=new Date().getMinutes();
+    let timesRun=0;
+    let Start=setInterval(function () {
+        timesRun +=1;
+        if(timesRun>10 || new Date().getMinutes()!==minutes){
+            clearInterval(Start)
+        }
+        else {
+            mul.value *=2;
+        }
+    },5000)
 }
 //3. 判断输入框most里出现最多的字符，并统计出来。统计出是信息在most_result输入框内以"The most character is:" + index + " times:" + max的形式显示。
 //如果多个出现数量一样则选择一个即可。
@@ -30,5 +45,26 @@ let result = document.getElementById("most-result");
 let most_submit = document.getElementById("most_submit");
 most_submit.addEventListener('click',arrSameStr);
 function arrSameStr(){
-
+ let obj={};
+    for (let i = 0; i < most.value.length ; i++) {
+        if(obj[most.value.charAt(i)]){
+            obj[most.value.charAt(i)]++;
+        }else{
+            obj[most.value.charAt(i)]=1;
+        }
+    }
+    let index='';
+    let max=0;
+    for(let answer in obj){
+        if(max<obj[answer]){
+            max=obj[answer];
+        }
+    }
+    for(let answer in obj){
+        if(obj[answer]===max){
+            index=answer;
+        }
+    }
+    result.value="The most character is:" + index + " times:" + max;
+    // console.log(obj);
 }
