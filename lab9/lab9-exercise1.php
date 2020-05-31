e710b858a6e26d94dd6aa7b4e1aa2ebef480a698
ZhaoRui-A <516911867@qq.com>
2020-05-31 23:07:24 +0800

    lab9

--- lab9/lab9-exercise1.php
+++ lab9/lab9-exercise1.php
@@ -9,9 +9,11 @@
 <h1>PHP section (inside &lt;?php ... ?&gt; tags)</h1>
 <?php
 //this is a php comment IN tags (will not appear)
-echo "This was output using PHP";
+$remaining = 365 - date("z");
+$year=date('Y');
+if(($year%100!==0 && $year%4===0) || $year%400===0) $remaining++;
+echo "There are ". $remaining . " days left in the year";
 echo "<br>"; //notice we must echo tags in php.
-
 ?>
 </body>
 </html>
